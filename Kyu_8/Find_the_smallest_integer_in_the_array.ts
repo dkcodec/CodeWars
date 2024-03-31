d27387772ca106bf90d3285ab38e2db38af07ff2
Kairgeldin Dmitry <dimgxj@gmail.com>
2024-03-31 13:58:54 +0500

    New Tasks

---
+++ Kyu_8/Find_the_smallest_integer_in_the_array.ts
@@ -0,0 +1,18 @@
+/*
+
+    Find the smallest integer in the array
+
+    Given an array of integers your solution should find the smallest integer.
+
+    For example:
+        Given [34, 15, 88, 2] your solution will return 2
+        Given [34, -345, -1, 100] your solution will return -345
+        You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+*/
+
+export function findSmallestInt(args: number[]): number {
+  return Math.min(...args.map((a) => a));
+
+  throw new Error("This method or operation is not implemented.");
+}
