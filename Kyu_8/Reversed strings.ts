a1beda7797efba8def8d893e1238dcd45977ba84
Kairgeldin Dmitry <dimgxj@gmail.com>
2024-04-02 22:25:22 +0500

    New Task

---
+++ Kyu_8/Reversed strings.ts
@@ -0,0 +1,13 @@
+/*
+
+    Reversed Strings
+
+    Complete the solution so that it reverses the string passed into it.
+
+    'world'  =>  'dlrow'
+    'word'   =>  'drow'
+*/
+
+export function solution(str: string): string {
+  return str.split("").reverse().join("");
+}
