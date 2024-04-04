b74a0402fe91992b441f191fc9e83d44262ecc0b
Kairgeldin Dmitry <dimgxj@gmail.com>
2024-04-04 20:28:28 +0500

    New Task *-*

---
+++ Kyu_5/Perimeter of squares in a rectangle.ts
@@ -0,0 +1,36 @@
+/*
+
+    Perimeter of squares in a rectangle
+
+    The drawing shows 6 squares the sides of which have a length 
+    of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters 
+    of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+    Could you give the sum of the perimeters of all the squares in 
+    a rectangle when there are n + 1 squares disposed in the same manner.
+
+    Hint:
+        See Fibonacci sequence
+
+    Ref:
+        http://oeis.org/A000045
+
+    The function perimeter has for parameter n where 
+    n + 1 is the number of squares (they are numbered from 0 to n) 
+    and returns the total perimeter of all the squares.
+
+        perimeter(5)  should return 80
+        perimeter(7)  should return 216
+*/
+
+export const perimeter = (n: number): number => {
+  let f = 1;
+  let s = 1;
+  let sum = 2;
+  for (let i = 3; i <= n + 1; i++) {
+    let c = f + s;
+    f = s;
+    s = c;
+    sum += s;
+  }
+  return sum * 4;
+};
